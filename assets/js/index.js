45feb8c1963a5c91b483ee79ae95a1d1640e5c9a
pooja <pooja1.img@gmail.com>
2023-05-08 16:36:32 +0530

    commit by pooja
    design testimonial section

--- assets/js/index.js
+++ assets/js/index.js
@@ -261,11 +261,24 @@ if(document.querySelector('[page-name=latestNews]')) {
         spaceBetween: 20,
       },
       1400: {
-        slidesPerView: 3,
+        slidesPerView: 4,
         spaceBetween: 20,
       },
     }
   });
+
+  var swiper = new Swiper(".testimonialSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
 }
 
 
